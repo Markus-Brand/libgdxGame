238b9f65b4c2791de0add9373d340ab430fb8e93
Erik Brendel <erik.brendel@gmx.de>
2018-02-13 21:50:06 +0100

    Fix indent of comments

--- core/src/com/mb/game/WorkingTitleGame.java
+++ core/src/com/mb/game/WorkingTitleGame.java
@@ -56,30 +56,30 @@ public class WorkingTitleGame extends ApplicationAdapter {
 	private void addDynamicBody() {
 		// First we create a body definition
 		BodyDef bodyDef = new BodyDef();
-// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+		// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
 		bodyDef.type = BodyDef.BodyType.DynamicBody;
-// Set our body's starting position in the world
+		// Set our body's starting position in the world
 		bodyDef.position.set(2.6f, 13.5f);
 
-// Create our body in the world using our body definition
+		// Create our body in the world using our body definition
 		Body body = mWorld.createBody(bodyDef);
 
-// Create a circle shape and set its radius to 6
+		// Create a circle shape and set its radius to 6
 		CircleShape circle = new CircleShape();
 		circle.setRadius(1f);
 
-// Create a fixture definition to apply our shape to
+		// Create a fixture definition to apply our shape to
 		FixtureDef fixtureDef = new FixtureDef();
 		fixtureDef.shape = circle;
 		fixtureDef.density = 0.5f;
 		fixtureDef.friction = 0.4f;
 		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
 
-// Create our fixture and attach it to the body
+		// Create our fixture and attach it to the body
 		mFixture = body.createFixture(fixtureDef);
 
-// Remember to dispose of any shapes after you're done with them!
-// BodyDef and FixtureDef don't need disposing, but shapes do.
+		// Remember to dispose of any shapes after you're done with them!
+		// BodyDef and FixtureDef don't need disposing, but shapes do.
 		circle.dispose();
 	}
 
